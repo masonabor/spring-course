76ab56ab0cee7738488117fcc96d786996ea30ea
MartyniukYurii <145149346+masonabor@users.noreply.github.com>
2025-09-02 15:55:57 +0300

    end hello world

--- lab1/hello-world/src/main/java/com/lab1/helloworld/HelloWorldApplication.java
+++ lab1/hello-world/src/main/java/com/lab1/helloworld/HelloWorldApplication.java
@@ -4,18 +4,17 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
 
 @SpringBootApplication
-public class HelloWorldApplication {
+public class HelloWorldApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(HelloWorldApplication.class, args);
     }
 
-    @Bean
-    public CommandLineRunner runner() {
-        return args -> {
-            System.out.println("Hello World");
-        };
+    @Override
+    public void run(String... args) {
+        System.out.println("Hello World!");
     }
 }
---
+++ lab1/hello-world/src/main/java/com/lab1/helloworld/Second.java
@@ -0,0 +1,15 @@
+package com.lab1.helloworld;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(2)
+public class Second implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) {
+        System.out.println("Second");
+    }
+}
