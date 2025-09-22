7f2181f4936f29e2592ff32680c3e9c218ca514e
MartyniukYurii <145149346+masonabor@users.noreply.github.com>
2025-09-22 17:35:54 +0300

    for rebase with fixErrors

--- lab1/hello-world/src/main/java/com/lab1/helloworld/HelloWorldApplication.java
+++ lab1/hello-world/src/main/java/com/lab1/helloworld/HelloWorldApplication.java
@@ -3,14 +3,14 @@ package com.lab1.helloworld;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.annotation.Order;
 
 @SpringBootApplication
 public class HelloWorldApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
+        System.out.println("start");
         SpringApplication.run(HelloWorldApplication.class, args);
+        System.out.println("finish");
     }
 
     @Override
